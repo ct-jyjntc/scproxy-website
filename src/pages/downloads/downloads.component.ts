b26a03bd4553331c94da43271f13407748bb0ff2
ct-jyjntc <copilotteam@hotmail.com>
2025-12-10 04:52:16 +0800

    feat(downloads): implement platform detection and update download links with query parameters

--- src/pages/downloads/downloads.component.ts
+++ src/pages/downloads/downloads.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectionStrategy, signal, computed, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { LanguageService, TranslationKey } from '../../services/language.service';
 import { ObserveElementDirective } from '../../directives/observe-element.directive';
 
@@ -20,6 +21,7 @@ interface AppInfo {
 })
 export class DownloadsComponent {
   languageService = inject(LanguageService);
+  route = inject(ActivatedRoute);
   activeTab = signal<'desktop' | 'mobile'>('desktop');
   selectedOsId = signal<string>('windows');
 
@@ -70,6 +72,14 @@ export class DownloadsComponent {
     return apps.find(app => app.id === this.selectedOsId()) ?? apps[0];
   });
 
+  constructor() {
+    const queryOs = this.route.snapshot.queryParamMap.get('os');
+    if (queryOs && this.setInitialSelection(queryOs)) {
+      return;
+    }
+    this.setInitialSelection(this.selectedOsId());
+  }
+
   selectTab(tab: 'desktop' | 'mobile') {
     this.activeTab.set(tab);
     this.selectedOsId.set(tab === 'desktop' ? 'windows' : 'android');
@@ -77,5 +87,21 @@ export class DownloadsComponent {
 
   selectOs(osId: string) {
     this.selectedOsId.set(osId);
+  }
+
+  private setInitialSelection(osId: string): boolean {
+    const isDesktop = this.desktopApps.some(app => app.id === osId);
+    const isMobile = this.mobileApps.some(app => app.id === osId);
+    if (isDesktop) {
+      this.activeTab.set('desktop');
+      this.selectedOsId.set(osId);
+      return true;
+    }
+    if (isMobile) {
+      this.activeTab.set('mobile');
+      this.selectedOsId.set(osId);
+      return true;
+    }
+    return false;
   }
 }
--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, signal, computed } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LanguageService } from '../../services/language.service';
 import { ObserveElementDirective } from '../../directives/observe-element.directive';
+
+type PlatformId = 'windows' | 'macos' | 'android' | 'ios' | 'other';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +14,7 @@ import { ObserveElementDirective } from '../../directives/observe-element.direct
 })
 export class HomeComponent {
     languageService = inject(LanguageService);
+    platform = signal<PlatformId>('macos');
 
     features = [
       {
@@ -36,4 +39,51 @@ export class HomeComponent {
       }
     ];
 
+    primaryDownloadId = computed(() => {
+      switch (this.platform()) {
+        case 'windows':
+          return 'windows';
+        case 'android':
+          return 'android';
+        case 'ios':
+          return 'ios';
+        default:
+          return 'macos';
+      }
+    });
+
+    primaryDownloadLabel = computed(() => {
+      switch (this.primaryDownloadId()) {
+        case 'windows':
+          return this.languageService.t('download_windows')();
+        case 'android':
+          return this.languageService.t('download_android')();
+        case 'ios':
+          return this.languageService.t('download_ios')();
+        default:
+          return this.languageService.t('download_macos')();
+      }
+    });
+
+    constructor() {
+      this.detectPlatform();
+    }
+
+    private detectPlatform() {
+      if (typeof navigator === 'undefined') {
+        return;
+      }
+      const ua = navigator.userAgent.toLowerCase();
+      if (ua.includes('windows')) {
+        this.platform.set('windows');
+      } else if (ua.includes('mac os') || ua.includes('macintosh')) {
+        this.platform.set('macos');
+      } else if (ua.includes('android')) {
+        this.platform.set('android');
+      } else if (ua.includes('iphone') || ua.includes('ipad') || ua.includes('ipod')) {
+        this.platform.set('ios');
+      } else {
+        this.platform.set('other');
+      }
+    }
 }
